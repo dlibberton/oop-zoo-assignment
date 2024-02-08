5168b8b1f2b2967c5e99d03ad8a12e9464029665
dlibberton <drew.libberton@gmail.com>
2024-02-08 13:09:21 -0500

    passes all tests

--- zoo_manager.py
+++ zoo_manager.py
@@ -2,21 +2,20 @@ class Animal:
     def __init__(self, name, species):
         self.name = name
         self.species = species
-        
 
     def speak(self):
-        print("Animal sound")
+        return("Animal sound")
 
 class Mammal(Animal):
     def __init__(self, name, species):
         super().__init__(name, species)
 
     def give_birth(self):
-        print(f"{self.name} the {self.species}has given birth")
+        return(f"{self.name} the {self.species} has given birth")
 
 class Bird(Animal):
     def __init__(self, name, species, wingspan):
-        super().__init__(name, species, wingspan)
+        super().__init__(name, species)
         self.wingspan = wingspan
 
 class Reptile(Animal):
@@ -24,21 +23,21 @@ class Reptile(Animal):
         super().__init__(name, species)
 
     def bask_in_sun(self):
-        print(f"{self.name} the {self.species} is basking in the sun")
+        return(f"{self.name} the {self.species} is basking in the sun")
 
 class Primate(Mammal):
     def __init__(self, name, species):
         super().__init__(name, species)
 
     def climb_trees(self):
-        print(f"{self.name} the {self.species} is climbing trees")
+        return(f"{self.name} the {self.species} is climbing trees")
 
 class Marsupial(Mammal):
     def __init__(self, name, species):
         super().__init__(name, species)
 
     def carry_baby(self):
-        print(f"{self.name} the {self.species} is carrying its baby")
+        return(f"{self.name} the {self.species} is carrying its baby")
 
 class Aviary():
     def __init__(self):
